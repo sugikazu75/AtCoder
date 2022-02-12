5fee9da9aef48c9bf7e1409e439a267bcac1b727
sugikazu75 <64698144+sugikazu75@users.noreply.github.com>
2022-02-12 22:08:27 +0900

    delete editable files from remote

--- test.py
+++
@@ -1,5 +0,0 @@
-s = input().split()
-a = s[0]
-b = s[1]
-
-print(s)
--- test_code2.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-using namespace std;
-#define rep(i, n) for(ll i = 0; i < n; i++)
-#define all(v) v.begin(), v.end()
-typedef long long ll;
-
-int main(){
-    
-}
