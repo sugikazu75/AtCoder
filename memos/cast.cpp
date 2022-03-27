f1141cf95877c0c579df06f0879cc19da429a096
sugikazu75 <64698144+sugikazu75@users.noreply.github.com>
2022-03-28 07:57:34 +0900

    debug

--- memos/cast.cpp
+++ memos/cast.cpp
@@ -4,19 +4,20 @@ using namespace std;
 #define all(v) v.begin(), v.end()
 
 int main(){
-    //数値型から文字列
+    //int to string
     int i1=100;
     string str1 = to_string(i1);
 
+    //string to int
+    string str4 = "100";
+    int i4 = stoi(str4);
+
     //int to char
-    int i2 = 100;
+    int i2 = 5;
     char ch2 = i2 + '0';
-    
+
     //char sからint 
     char ch3 = '9';
     int i3 = ch3 - '0';
-
-    //stringからint
-    string str4 = "100";
-    int i4 = stoi(str4);
+    
 }
